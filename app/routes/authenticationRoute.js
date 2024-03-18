23541fe995fb459324fefc6618634a1c0ecf5f09
Warren <w.gaure@gmail.com>
2024-03-18 13:26:11 +0100

    [feat] Creating the router for authentification

--- app/routes/authenticationRoute.js
+++ app/routes/authenticationRoute.js
@@ -1,43 +1,19 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
-const user = require('../models/userModel');
+/**
+ * Le fichier contenant les routes d'authentification.
+ * @author GAURE Warren
+ * @version 1.0
+*/
 
 const router = express.Router();
+const authenticationController = require('../controllers/authenticationController');
 
-// POST /register - User registration endpoint
-router.post('/register', async (req, res) => {
-  try {
-    // On récupère les infos de l'utilisateur depuis le body de la requête
-    const { username, email, password, firstName, lastName } = req.body;
-
-    // On vérifie si l'utilisateur existe déjà
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-        return res.status(400).json({ error: "L'utilisateur existe déjà" });
-    }
-
-    // On hash le mot de passe
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    // On créé un nouvel utilisateur
-    const newUser = new user({
-      username: username,
-      email: password,
-      password: hashedPassword,
-      firstName: firstName,
-      lastName: lastName
-    });
-
-    // On ajoute le nouvel utilisateur à la base de données
-    await newUser.save();
-
-    // On renvoie un message à l'utilisateur
-    res.status(201).json({ message: "Inscription déroulée avec succès" });
-  }
-  catch (error) {
-    console.error("Erreur pendant l'inscription d'un utilisateur : ", error);
-    res.status(500).json({ error: 'Inscription échouée' });
-  }
-});
+// Route pour se connecter
+router.post('/login', authenticationController.login);
+// Route pour se déconnecter
+router.post('/logout', authenticationController.logout);
+// Route pour s'inscrire
+router.post('/register', authenticationController.register);
+// Route pour rafraîchir le token
+router.post('/refreshToken', authenticationController.refreshToken);
 
 module.exports = router;
