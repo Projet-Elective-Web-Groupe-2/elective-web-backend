da7d3d3bb1e3859221d36d92847480e891752b4f
Nicolas <93578555+keysha18@users.noreply.github.com>
2024-03-25 10:29:54 +0100

    fix(Automatisation) : fixed release (hopefully)

--- release.config.js
+++ release.config.js
@@ -1,12 +1,19 @@
 module.exports = {
-    branches: ['main', 'feat/NewDeploySemanticRelease'], // Ajoutez la branche de fonctionnalité à surveiller pour les nouvelles versions
-    plugins: [
-      '@semantic-release/commit-analyzer', // Analyse les messages de commit pour déterminer le type de version
-      '@semantic-release/release-notes-generator', // Génère automatiquement les notes de publication
-      '@semantic-release/changelog', // Génère automatiquement le journal des modifications
-      ['@semantic-release/git', {
-        assets: ['package.json', 'CHANGELOG.md'], // Fichiers à inclure dans les commits de version
-      }],
+  branches: ['main', 'feat/NewDeploySemanticRelease'], // Ajoutez la branche de fonctionnalité à surveiller pour les nouvelles versions
+  plugins: [
+    '@semantic-release/commit-analyzer', // Analyse les messages de commit pour déterminer le type de version
+    '@semantic-release/release-notes-generator', // Génère automatiquement les notes de publication
+    '@semantic-release/changelog', // Génère automatiquement le journal des modifications
+    ['@semantic-release/git', {
+      assets: ['package.json', 'CHANGELOG.md'], // Fichiers à inclure dans les commits de version
+    }],
+  ],
+  // Définir une plage de versions autorisées
+  preset: 'conventionalcommits',
+  release: {
+    branches: [
+      { name: 'main', prerelease: true },
+      { name: 'feat/NewDeploySemanticRelease', prerelease: true },
     ],
-  };
-  
+  },
+};
