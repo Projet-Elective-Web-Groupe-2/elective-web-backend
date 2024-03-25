1af9037ca730e97ccb6eda39239412979539b94b
Nicolas <93578555+keysha18@users.noreply.github.com>
2024-03-25 14:57:30 +0100

    feat(Automatisation): added automatic version in package.json

--- release.config.js
+++ release.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  branches: ['main', 'develop'], // Ajoutez la branche de fonctionnalité à surveiller pour les nouvelles versions
+  branches: ['main', 'develop', 'feat/SemanticReleaseTesting'], // Ajoutez la branche de fonctionnalité à surveiller pour les nouvelles versions
   plugins: [
     '@semantic-release/commit-analyzer', // Analyse les messages de commit pour déterminer le type de version
     '@semantic-release/release-notes-generator', // Génère automatiquement les notes de publication
     '@semantic-release/changelog', // Génère automatiquement le journal des modifications
+    '@semantic-release/npm',
     ['@semantic-release/git', {
       assets: ['package.json', 'CHANGELOG.md'], // Fichiers à inclure dans les commits de version
     }],
@@ -14,6 +15,7 @@ module.exports = {
     branches: [
       { name: 'main', prerelease: false }, // Modifier prerelease en false
       { name: 'develop', prerelease: false }, // Modifier prerelease en false
+      { name: 'feat/SemanticReleaseTesting', prerelease: false }, // Modifier prerelease en false
     ],
   },
 }  
