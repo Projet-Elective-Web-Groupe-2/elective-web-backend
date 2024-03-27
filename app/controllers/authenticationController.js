6ea3ed261c2986b57cf262e34d0bebafbcfae514
Warren <w.gaure@gmail.com>
2024-03-27 09:10:37 +0100

    feat(authentication): finished router, controller and service

--- app/controllers/authenticationController.js
+++ app/controllers/authenticationController.js
@@ -9,6 +9,7 @@ const jwt = require('jsonwebtoken');
 const User = require("../models/userModel");
 const authenticationService = require('../services/authenticationService');
 
+
 const login = async (req, res) => {
     // Vérification de la présence du body dans la requête
     if (!req.body) {
@@ -20,37 +21,46 @@ const login = async (req, res) => {
     const password = req.body["password"];
 
     if (!email || !password) {
-        return res.status(400).json({ error: "Missing mandatory data" });
+        return res.status(400).json({ error: "Missing mandatory data for verification" });
     }
 
     try {
-        // Vérification de l'existence de l'utilisateur dans la base de données
-        const existingUser = await authenticationService(email);
+        // Appel au service d'authentification pour vérifier l'existence de l'utilisateur dans la base de données
+        const existingUser = await authenticationService.findUserByEmail(email);
 
-        if (!existingUser) {
-            return res.status(400).json({ error: "User does not exist" });
-        }
-
-        // Vérification du mot de passe
-        const isPasswordCorrect = await bcrypt.compare(password + process.env.PEPPER_STRING, existingUser.password);
+        // Appel au service d'authentification pour vérifier le mot de passe
+        const isPasswordCorrect = await authenticationService.comparePassword(existingUser.password, password);
         
-        if (!isPasswordCorrect) {
-            return res.status(401).json({ error: "Invalid password" });
-        }
-
-        // Génération du token
-        const token = jwt.sign({ id: existingUser._id, userType: existingUser.userType }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        // Appel au service d'authentification pour générer le token
+        const token = authenticationService.generateJWT(existingUser.userID, existingUser.userType);
 
         return res.status(200).json({ token });
     }
     catch (error) {
-        console.error("Error while trying to login : ", error);
-        res.status(500).json({ error: "Login failed" });
+        if (error.message === "User not found") {
+            return res.status(404).json({ error : error.message });
+        }
+        else if (error.message === "Invalid password") {
+            return res.status(401).json({ error : error.message });
+        }
+        else {
+            console.error("Unexpected error while logging in : ", error);
+            res.status(500).json({ error: "Login failed" });
+        }
     }
 };
 
 const logout = (req, res) => {
-    res.status(200).json({ message: "Successfully disconnected" });
+    try {
+        // Suppression du token
+        res.clearCookie("token");
+
+        res.status(200).json({ message: "Logout successful" });
+    }
+    catch(error) {
+        console.error("Unexpected error while logging out : ", error);
+        res.status(500).json({ error: "Logout failed" });
+    }
 };
 
 const register = async (req, res) => {
@@ -59,45 +69,83 @@ const register = async (req, res) => {
     }
 
     const email = req.body["email"];
+    // La variable changera de valeur
     const password = req.body["password"];
     const userType = req.body["userType"];
+    const phoneNumber = req.body["phoneNumber"];
 
-    if (!email || !password ||!userType) {
-        return res.status(400).json({ error: "Missing mandatory data" });
+    if (!email || !password || !userType || !phoneNumber) {
+        return res.status(400).json({ error: "Missing mandatory data for registration" });
     }
-
-    const existingUser = authenticationService(email);
-    
-    if (existingUser) {
-        return res.status(400).json({ error: "User already exists" });
-    }
-            
-    // Cryptage du mot de passe
-    const hashedPassword = await bcrypt.hash(password + process.env.PEPPER_STRING, 10);
-
-    let newUser;
 
     try {
-        // On créé un nouvel utilisateur et on l'ajoute à la base de données
-        const newUser = new User({
-            firstName,
-            lastName,
-            email,
-            password: hashedPassword,
-        });
-        
-        await newUser.save();
-        
-        res.status(201).json({ message: "Successfully registered" });
+        // Vérification de l'existence de l'utilisateur dans la base de données
+        const existingUser = await authenticationService.findUserByEmail(email);
+    
+        if (existingUser) {
+            throw new Error("User already exists");
+        }
+
+        const encryptedPassword = await authenticationService.encryptPassword(password);
+
+        let newUser;
+
+        switch(userType) {
+            case "CLIENT":
+            case "LIVREUR": {
+                const firstName = req.body["firstName"];
+                const lastName = req.body["lastName"];
+                const address = req.body["address"];
+                
+                if (!firstName ||!lastName ||!address) {
+                    throw new Error("Missing mandatory data");
+                }
+
+                newUser = await authenticationService.createClientOrDeliverer(email, encryptedPassword, userType, firstName, lastName, address, phoneNumber);
+                
+                break;
+            }
+            case "RESTAURATEUR": {
+                newUser = await authenticationService.createRestaurateur(email, encryptedPassword, userType, phoneNumber);
+
+                // TODO : Implémenter la création d'un restaurant dans la base MongoDB
+
+                break;
+            }
+            case "DEVELOPPEUR TIERS": {
+                // TODO : Penser à modifier la méthode pour inclure la clé de sécurité
+                newUser = await authenticationService.createDeveloper(email, encryptedPassword, userType, phoneNumber);
+
+                break;
+            }
+            default: {
+                throw new Error("Invalid user type");
+            }
+        }
     }
-    catch (error) {
-        console.error("Erreur pendant l'inscription d'un utilisateur : ", error);
-        res.status(500).json({ error: 'Inscription échouée' });
+    catch(error) {
+        if (error.message === "User already exists") {
+            return res.status(400).json({ error : error.message });
+        }
+        else if (error.message === "Missing mandatory data") {
+            return res.status(400).json({ error: `Missing mandatory data to create a ${userType}` });
+        }
+        else if (error.message === "Invalid user type") {
+            return res.status(400).json({ error: "Invalid user type"});
+        }
+        else {
+            console.error("Unexpected error while registering : ", error);
+            res.status(500).json({ error: "Registration failed" });
+        }
     }
+
+    const token = authenticationService.generateJWT(newUser.userID, newUser.userType);
+
+    return res.status(200).json({ token });
 };
 
 module.exports = {
     login,
     logout,
-    register,
+    register
 };
--- app/services/authenticationService.js
+++ app/services/authenticationService.js
@@ -1,11 +1,100 @@
 /**
- * Le module contenant les requêtes liées à l'authentification.
+ * Le service contenant les requêtes liées à l'authentification.
  * @author GAURE Warren
  * @version 1.0
 */
 
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
+
+/**
+ * Fonction permettant de créér un client ou un livreur dans la base de données.
+ * @param {String} email - L'email du client / livreur.
+ * @param {String} password - Le mot de passe du client / livreur.
+ * @param {String} userType - Le type de l'utilisateur ("CLIENT" ou "LIVREUR").
+ * @param {String} firstName - Le prénom du client / livreur .
+ * @param {String} lastName - Le nom du client / livreur.
+ * @param {String} address - L'addresse du client / livreur.
+ * @param {String} phoneNumber - Le numéro de téléphone du client / livreur.
+ * @returns {object} Le client / livreur en question, ou false si rien n'a été créé.
+*/
+const createClientOrDeliverer = async (email, password, userType, firstName, lastName, address, phoneNumber) => {
+    try {
+        const newUser = new User({
+            userID: Math.floor(Math.random() * 100),
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: password,
+            phoneNumber: phoneNumber,
+            userType: userType,
+            address: address,
+            referralCode: (Math.random() + 1).toString(36).substring(2),
+        });
+
+        await newUser.save();
+
+        return newUser;
+    }
+    catch(error) {
+        throw new Error("Error while trying to create a client / deliverer in the database : " + error.message);
+    }
+}
+
+/**
+ * Fonction permettant de créer un restaurateur dans la base de données.
+ * @param {String} email - L'email du restaurateur.
+ * @param {String} password - Le mot de passe du restaurateur.
+ * @param {String} userType - Le type de l'utilisateur ("RESTAURATEUR").
+ * @param {String} phoneNumber - Le numéro de téléphone du restaurateur.
+ * @returns {object} Le restaurateur en question, ou false si rien n'a été créé.
+*/
+const createRestaurateur = async (email, password, userType, phoneNumber) => {
+    try {
+        const newUser = new User({
+            userID: Math.floor(Math.random() * 100),
+            email: email,
+            password: password,
+            phoneNumber: phoneNumber,
+            userType: userType,
+        });
+
+        await newUser.save();
+
+        return newUser;
+    }
+    catch(error) {
+        throw new Error("Error while trying to create a restaurateur in the database : " + error.message);
+    }
+};
+
+/**
+ * Fonction permettant de créer un développeur tiers dans la base de données.
+ * @param {String} email - L'email du développeur.
+ * @param {String} password - Le mot de passe du développeur.
+ * @param {String} userType - Le type de l'utilisateur ("DEVELOPPEUR TIERS").
+ * @param {String} phoneNumber - Le numéro de téléphone du développeur.
+ * @returns {object} Le développeur en question, ou false si rien n'a été créé.
+ */
+// TODO : Modifier la méthode pour générer la clé de sécurité et l'ajouter
+const createDeveloper = async (email, password, userType, phoneNumber) => {
+    try {
+        const newUser = new User({
+            userID: Math.floor(Math.random() * 100),
+            email: email,
+            password: password,
+            phoneNumber: phoneNumber,
+            userType: userType,
+        });
+
+        await newUser.save();
+
+        return newUser;
+    }
+    catch(error) {
+        throw new Error("Error while trying to create a developer in the database : " + error.message);
+    }
+};
 
 /**
  * Fonction permettant de récupérer un utilsateur depuis la base de données grâce à son email.
@@ -26,92 +115,29 @@ const findUserByEmail = async (email) => {
 };
 
 /**
- * Fonction permettant de créér un client ou un livreur dans la base de données.
- * @param {String} email - L'email du client / livreur.
- * @param {String} password - Le mot de passe du client / livreur.
- * @param {String} userType - Le type de l'utilisateur ("CLIENT" ou "LIVREUR").
- * @param {String} firstName - Le prénom du client / livreur .
- * @param {String} lastName - Le nom du client / livreur.
- * @param {String} address - L'addresse du client / livreur.
- * @param {String} phoneNumber - Le numéro de téléphone du client / livreur.
+ * Fonction permettant de crypter le mot de passe entré par l'utilisateur.
+ * @param {String} password - Le mot de passe à crypter.
+ * @returns {String} Le mot de passe crypté.
 */
-const createClientOrDeliverer = async (email, password, userType, firstName, lastName, address, phoneNumber) => {
-    try {
-        const newUser = new User({
-            userID: Math.floor(Math.random() * 100),
-            firstName: firstName,
-            lastName: lastName,
-            email: email,
-            password: password,
-            phoneNumber: phoneNumber,
-            userType: userType,
-            address: address,
-            referralCode: (Math.random() + 1).toString(36).substring(2),
-        });
+const encryptPassword = async(password) => {
+    const newPassword = await bcrypt.hash(password + process.env.PEPPER_STRING, 10);
 
-        await newUser.save();
-    }
-    catch(error) {
-        throw new Error("Error while trying to create a client / deliverer in the database : " + error.message);
-    }
+    return newPassword;
 }
-
-/**
- * Fonction permettant de créer un restaurateur dans la base de données.
- * @param {String} email - L'email du restaurateur.
- * @param {String} password - Le mot de passe du restaurateur.
- * @param {String} userType - Le type de l'utilisateur ("RESTAURATEUR").
- * @param {String} phoneNumber - Le numéro de téléphone du restaurateur.
-*/
-const createRestaurateur = async (email, password, userType, phoneNumber) => {
-    try {
-        const newUser = new User({
-            userID: Math.floor(Math.random() * 100),
-            email: email,
-            password: password,
-            phoneNumber: phoneNumber,
-            userType: userType,
-        });
-
-        await newUser.save();
-    }
-    catch(error) {
-        throw new Error("Error while trying to create a restaurateur in the database : " + error.message);
-    }
-};
-
-/**
- * Fonction permettant de créer un développeur tiers dans la base de données.
- * @param {String} email - L'email du développeur.
- * @param {String} password - Le mot de passe du développeur.
- * @param {String} userType - Le type de l'utilisateur ("DEVELOPPEUR TIERS").
- * @param {String} phoneNumber - Le numéro de téléphone du développeur.
- */
-const createDeveloper = async (email, password, userType, phoneNumber) => {
-    try {
-        const newUser = new User({
-            userID: Math.floor(Math.random() * 100),
-            email: email,
-            password: password,
-            phoneNumber: phoneNumber,
-            userType: userType,
-        });
-
-        await newUser.save();
-    }
-    catch(error) {
-        throw new Error("Error while trying to create a developer in the database : " + error.message);
-    }
-};
 
 /**
  * Fonction permettant de vérifier si le mot de passe de l'utilisateur est correct.
  * @param {String} password - Le mot de passe de l'utilisateur dans la base de données.
  * @param {String} passwordToVerify - Le mot de passe à vérifier.
- * @returns {boolean} True si le mot de passe est correct, false sinon.
+ * @returns {boolean} True si le mot de passe est correct, sinon une erreur est lancée.
 */
 const comparePassword = async (password, passwordToVerify) => {
     const isPasswordCorrect = await bcrypt.compare(passwordToVerify + process.env.PEPPER_STRING, password);
+
+    if (!isPasswordCorrect) {
+        throw new Error("Invalid password");
+    }
+
     return isPasswordCorrect;
 };
 
@@ -127,10 +153,11 @@ const generateJWT = (userID, userType) => {
 };
 
 module.exports = {
-    findUserByEmail,
     createClientOrDeliverer,
     createRestaurateur,
     createDeveloper,
+    findUserByEmail,
+    encryptPassword,
     comparePassword,
     generateJWT
 };
