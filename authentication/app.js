a446f9ee31583f43967101ee49eb216805151d41
Warren <w.gaure@gmail.com>
2024-03-28 22:48:31 +0100

    progress: fixing stuff all around

--- authentication/app.js
+++ authentication/app.js
@@ -4,13 +4,23 @@
  * @version 1.0
 */
 
+const express = require('express');
 const mongoose = require('mongoose');
 const mongoSanitize = require('express-mongo-sanitize');
+const cors = require('cors');
+
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDoc = YAML.load('./swagger.yaml');
+
+const loggerMiddleware = require('./app/middlewares/loggerMiddleware');
+
+const authenticationRouter = require('./app/routers/authenticationRouter');
 
 require("dotenv").config();
 
-const app = require('./config/appConfig');
-const authenticationRouter = require('./app/routers/authenticationRouter');
+const app = express();
+
 const port = process.env.AUTH_PORT || 3000;
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -21,7 +31,14 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
     console.error("Error while connecting to MongoDB : ", error);
 });
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(mongoSanitize());
+app.use(cors());
+
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
+
+app.use(loggerMiddleware);
 
 app.use('/auth', authenticationRouter);
 
