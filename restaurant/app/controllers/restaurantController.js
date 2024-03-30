c47cd162d1513ac42bf62dc25363422ee881efd6
Amara-ahmed <ahmed.amara606@gmail.com>
2024-03-30 16:35:02 +0100

    feat:add restaurant controller

---
+++ restaurant/app/controllers/restaurantController.js
@@ -0,0 +1,25 @@
+const Restaurant = require('../models/restaurantModel');
+
+exports.createRestaurant = async (req, res) => {
+    try {
+        if (!req.body.name || !req.body.address || !req.body.phoneNumber) {
+            throw new Error('Missing required fields');
+        }
+
+        const newRestaurant = new Restaurant({
+            name: req.body.name,
+            address: req.body.address,
+            phoneNumber: req.body.phoneNumber,
+            image: req.body.image,
+            ownerID: req.body.ownerID,
+            description: req.body.description,
+            status: req.body.status,
+        });
+
+        const savedRestaurant = await newRestaurant.save();
+
+        res.status(201).send(savedRestaurant); // ajouté temporellement pour le debogage 
+    } catch (err) {
+        res.sendStatus(400); 
+    }
+};
