fcf6691cd50677b134004aa4d295234b94d58451
Amara-ahmed <ahmed.amara606@gmail.com>
2024-03-30 22:44:39 +0100

    fix app.js product

--- product/app.js
+++ product/app.js
@@ -1,15 +1,18 @@
 /**
- * Le fichier principal du microservice des articles.
- * @author GAURE Warren
+ * Le fichier principal du microservice des restaurants.
+ * @author GAURE Warren, Amara Ahmed
  * @version 1.0
 */
 
 const express = require('express');
 const cors = require('cors');
-
+const mongoSanitize = require('express-mongo-sanitize');
 const swaggerUI = require('swagger-ui-express');
 const YAML = require('yamljs');
 const swaggerDoc = YAML.load('./swagger.yaml');
+const restaurantRoutes = require('./app/routers/restaurantRouter');
+const mongoose = require('mongoose');
+
 
 const loggerMiddleware = require('./app/middlewares/loggerMiddleware');
 const authenticationMiddleware = require('./app/middlewares/authenticationMiddleware');
@@ -18,14 +21,25 @@ require("dotenv").config();
 
 const app = express();
 
-const port = process.env.PRODUCT_PORT || 3004;
-
+const port = process.env.RESTAURANT_PORT || 3004;
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => {
+    console.log("Connected to MongoDB");
+})
+.catch((error) => {
+    console.error("Error while connecting to MongoDB : ", error);
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(mongoSanitize());
 app.use(cors());
 
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
+app.post('/test', (req, res) => {
+    res.status(200).send('Test endpoint is working');
+  });  
+
+
 
 app.use(loggerMiddleware);
 app.use(authenticationMiddleware);
