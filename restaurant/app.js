9d4a83992cd0bad190360ca8b1f2d1120aae7394
Amara-ahmed <ahmed.amara606@gmail.com>
2024-03-30 22:41:56 +0100

    feat:restaurant creatin working

--- restaurant/app.js
+++ restaurant/app.js
@@ -10,8 +10,8 @@ const mongoSanitize = require('express-mongo-sanitize');
 const swaggerUI = require('swagger-ui-express');
 const YAML = require('yamljs');
 const swaggerDoc = YAML.load('./swagger.yaml');
-const mongoSanitize = require('express-mongo-sanitize');
 const restaurantRoutes = require('./app/routers/restaurantRouter');
+const mongoose = require('mongoose');
 
 
 const loggerMiddleware = require('./app/middlewares/loggerMiddleware');
@@ -22,13 +22,22 @@ require("dotenv").config();
 const app = express();
 
 const port = process.env.RESTAURANT_PORT || 3002;
-
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => {
+    console.log("Connected to MongoDB");
+})
+.catch((error) => {
+    console.error("Error while connecting to MongoDB : ", error);
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(mongoSanitize());
 app.use(cors());
 
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
+app.post('/test', (req, res) => {
+    res.status(200).send('Test endpoint is working');
+  });  
 app.use('/restaurants', restaurantRoutes);
 
 
--- restaurant/app/controllers/restaurantController.js
+++ restaurant/app/controllers/restaurantController.js
@@ -1,25 +1,32 @@
 const Restaurant = require('../models/restaurantModel');
 
 exports.createRestaurant = async (req, res) => {
+    console.log("Received request:", req.body);
     try {
+        // Vérification des champs requis
         if (!req.body.name || !req.body.address || !req.body.phoneNumber) {
+            console.log("Validation error: Missing required fields");
             throw new Error('Missing required fields');
         }
 
+        console.log("Creating new restaurant with data:", req.body);
         const newRestaurant = new Restaurant({
             name: req.body.name,
             address: req.body.address,
             phoneNumber: req.body.phoneNumber,
-            image: req.body.image,
-            ownerID: req.body.ownerID,
-            description: req.body.description,
-            status: req.body.status,
+            image: req.body.image || '', 
+            ownerID: req.body.ownerID || null,
+            description: req.body.description || '', 
+            status: req.body.status || false, 
         });
 
+      
         const savedRestaurant = await newRestaurant.save();
+        console.log("Restaurant saved:", savedRestaurant);
 
-        res.status(201).send(savedRestaurant); // ajouté temporellement pour le debogage 
+        res.status(201).send(savedRestaurant);
     } catch (err) {
-        res.sendStatus(400); 
+        console.log("Error in createRestaurant:", err.message);
+        res.status(400).send({ error: err.message });
     }
 };
