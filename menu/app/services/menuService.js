e8207635398e8e2375ad348f5087c511956e8ee1
Amara-ahmed <ahmed.amara606@gmail.com>
2024-04-04 18:03:59 +0200

    menu service

---
+++ menu/app/services/menuService.js
@@ -0,0 +1,26 @@
+const menu = require('../models/menuModel');
+
+const createMenu = async (name, products) => {
+    try {
+        const totalPrice = products.reduce((acc, product) => acc + product.price, 0);
+
+        const description = products.map(product => product.name).join(', ');
+
+        const newMenu = new menu({ 
+            name: name,
+            description: description,
+            totalPrice: totalPrice
+        });
+
+        await newMenu.save();
+
+        return newMenu;
+    }
+    catch (error) {
+        throw new Error("Error while trying to create a Menu : " + error.message)
+    }
+}
+
+module.exports = {
+    createMenu,
+};
