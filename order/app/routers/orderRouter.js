18bff1aef9c9a4e7b2fd5dce8d478d7e89e10072
Warren <w.gaure@gmail.com>
2024-04-04 10:27:08 +0200

    progress: router done

---
+++ order/app/routers/orderRouter.js
@@ -0,0 +1,20 @@
+/**
+ * Le routeur pour les commandes.
+ * @author GAURE Warren
+ * @version 1.0
+*/
+
+const express = require('express');
+const router = express.Router();
+const orderController = require('../controllers/orderController');
+
+router.post('/create', orderController.createAndAddOrder);
+router.post('/addMenu', orderController.addMenu);
+router.post('/addProduct', orderController.addProduct);
+router.get('/get', orderController.getOrder);
+router.get('/ordersOfUser', orderController.listOrdersFromUser);
+routeur.get('/ordersOfRestaurant', orderController.listOrdersFromRestaurant);
+router.delete('/delete', orderController.deleteOrder);
+router.get('/metrics', orderController.metrics);
+
+module.exports = router;
