4b4d9dcd1587494c5c748137272237db14a5768a
Amara-ahmed <ahmed.amara606@gmail.com>
2024-04-07 00:47:07 +0200

    add drink to menu schema

--- menu/app/schemas/menuSchema.js
+++ menu/app/schemas/menuSchema.js
@@ -34,6 +34,10 @@ const menuSchema = new mongoose.Schema({
         type: Number,
         unique: false,
         required: true
+    },
+    drink: {
+        type: Boolean,
+        default: false
     }
 });
 
