6260d5c008e065f6afb138d49812b8c926f74d51
Warren <w.gaure@gmail.com>
2024-04-10 14:56:10 +0200

    hotfix again sa mère

--- menu/app/controllers/menuController.js
+++ menu/app/controllers/menuController.js
@@ -24,7 +24,7 @@ const createAndAddMenu = async (req, res) => {
     const drinkButtonClicked = req.body["drinkButtonClicked"];
     const drink = drinkButtonClicked ? true : false;
 
-    if (!name || !restaurantID || !productIds || !image || !totalPrice || drinkButtonClicked === undefined || drinkButtonClicked === null) {
+    if (!name || !restaurantID || !productIds || !image || !totalPrice) {
         return res.status(400).json({ error: "Missing mandatory data to create menu" });
     }
 
