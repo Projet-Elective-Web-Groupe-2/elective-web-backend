6840383b5929ddbcd4e2d5f0d69c7ffb5be0084d
Warren <w.gaure@gmail.com>
2024-04-10 14:24:58 +0200

    progress: wip

--- components/app/controllers/componentsController.js
+++ components/app/controllers/componentsController.js
@@ -4,6 +4,7 @@
  * @version 1.0
 */
 
+const axios = require('axios')
 const componentsService = require('../services/componentsService');
 
 const writeLogs = async (req, res) => {
@@ -13,14 +14,70 @@ const writeLogs = async (req, res) => {
     const componentName = req.body.componentName;
 
     if (userType != "DEVELOPER") {
+        return res.status(403).send({ message: "Forbidden" });
+    }
+    else if (!componentName) {
+        return res.status(400).send({ message: "Missing component name" });
+    }
 
-    await componentsService.writeLogs(userID, componentName);
-    res.status(200).send('Logs written successfully!');
+    try {
+        const url = `${AUTH_URL}find`;
+        const response = await axios.get(url, {
+            params: { id: userID },
+            headers: { Authorization: `Bearer ${token}` }
+        });
+
+        if (response.status !== 200) {
+            throw new Error("User not found");
+        }
+
+        await componentsService.writeLogs(userID, componentName);
+
+        return res.status(200).send('Logs written successfully!');
+    }
+    catch (error) {
+        if (error.message === "User not found") {
+            return res.status(404).send({ error: error.message });
+        }
+        else {
+            console.error("Unexpected error while writing logs : ", error);
+            return res.status(500).send({ error: "Internal server error" });
+        }
+    }
 };
 
-const getLogs = (req, res) => {
-    const logs = componentsService.getLogs();
-    res.status(200).send(logs);
+const getLogs = async (req, res) => {
+    const token = req.headers.authorization.split(' ')[1];
+    const userID = req.decoded.id;
+    const userType = req.decoded.type;
+
+    if (userType != "TECHNICAL ") {
+        return res.status(403).send({ message: "Forbidden" });
+    }
+    try {
+        const url = `${AUTH_URL}find`;
+        const response = await axios.get(url, {
+            params: { id: userID },
+            headers: { Authorization: `Bearer ${token}` }
+        });
+
+        if (response.status !== 200) {
+            throw new Error("User not found");
+        }
+
+        const logs = await componentsService.getLogs();
+
+        return res.status(200).json({ logs });
+    }
+    catch (error) {
+        if (error.message === "User not found") {
+            return res.status(404).send({ error: error.message });
+        }
+        else {
+            console.error("Unexpected error while getting logs : ", error);
+            return res.status(500).send({ error: "Internal server error" });
+        }
+    }
 };
 
 const metrics = async (req, res) => {
